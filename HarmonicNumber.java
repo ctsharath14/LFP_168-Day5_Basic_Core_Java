5c7cbb86094c1321f0c0abd77d083c0859222d07
ctshara14 <ctsharath14@gmail.com>
2022-07-07 17:34:12 +0530

    Adding: Program to print the Nth Harmonic value

---
+++ HarmonicNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class HarmonicNumber {
+	public static void main(String[] args) {
+		
+		double num,i;
+		double sum=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number");
+		num=sc.nextDouble();
+		
+		for( i=1;i<=num;i++)
+		{
+			sum=sum+(1/i);
+		}
+		System.out.println("Harmonic value = "+sum);
+	}
+}
